a76916153b88cfd1ff8b8131bab06761a97e271b
CoolandWild <eric.boileaux@sncf.fr>
2020-11-09 01:08:48 +0100

    first push

---
+++ Adress.java
@@ -0,0 +1,8 @@
+public class Adress {
+    public static void main(String[] args) { 
+    String number;
+    String street;
+    String city;
+    String country;
+    }
+}
---
+++ User.java
@@ -0,0 +1,12 @@
+import java.sql.Date;
+public class User {
+
+    public static void main(String[] args, Object[] Photo,String Adress) {
+        
+    
+    Date birthdate;
+    int size ;
+    Object[] photo = Photo;
+    String adress= Adress; 
+    }
+}
